71aac6978c9a9c2355375054f9b58d46deae257a
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-05-03 15:11:50 -0300

    questão 03

---
+++ Lista 2/questao_03.py
@@ -0,0 +1,9 @@
+qntpessoas = int(input("Qual o número de pessoas? "))
+
+soma = 0
+for i in range(qntpessoas):
+    idades = int(input("Informe as idades: "))
+    soma = soma + idades
+
+media = soma // qntpessoas
+print("A média de idade das pessoas é " + str(media) + " anos")
