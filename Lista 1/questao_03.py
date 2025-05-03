7045daeea269e33324412c148a1e09c58fc0162b
Maria Helena <helena.varelakg@gmail.com>
2025-05-03 11:01:41 -0300

    questao 3

--- Lista 1/questao_03.py
+++ Lista 1/questao_03.py
@@ -1,19 +1,19 @@
-larg1 = int(input("Qual a largura do lote 1? "))
-comp1 = int(input("Qual o comprimento do lote 1? "))
+larg1 = float(input("Qual a largura do lote 1? "))
+comp1 = float(input("Qual o comprimento do lote 1? "))
 
-larg2 = int(input("Qual a largura do lote 2? "))
-comp2 = int(input("Qual o comprimento do lote 2? "))
+larg2 = float(input("Qual a largura do lote 2? "))
+comp2 = float(input("Qual o comprimento do lote 2? "))
 
-larg3 = int(input("Qual a largura do lote 3? "))
-comp3 = int(input("Qual o comprimento do lote 3? "))
+larg3 = float(input("Qual a largura do lote 3? "))
+comp3 = float(input("Qual o comprimento do lote 3? "))
 
-larg4 = int(input("Qual a largura do lote 4? "))
-comp4 = int(input("Qual o comprimento do lote 4? "))
+larg4 = float(input("Qual a largura do lote 4? "))
+comp4 = float(input("Qual o comprimento do lote 4? "))
 
 area1 = larg1 * comp1
 area2 = larg2 * comp2
 area3 = larg3 * comp3
-area4 = larg4 * larg4
+area4 = larg4 * comp4
 areatotal = area1 + area2 + area3 + area4
 
 print(f"A área total do terreno é {areatotal}")
